27a1f50920eecdb889928a65c27e639addbe1382
Phong Trần <phongtt7@gmail.com>
2016-05-26 23:54:09 +0700

    final
    final

--- bansung/GameScene.swift
+++ bansung/GameScene.swift
@@ -9,33 +9,57 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var robot:SKSpriteNode!
+    var bullet:SKSpriteNode!
+    let r1:SKTexture = SKTexture(imageNamed: "1.png")
+    let r2:SKTexture = SKTexture(imageNamed: "2.png")
+    let r3:SKTexture = SKTexture(imageNamed: "3.png")
+    let r4:SKTexture = SKTexture(imageNamed: "4.png")
+    let r5:SKTexture = SKTexture(imageNamed: "5.png")
+    let r6:SKTexture = SKTexture(imageNamed: "6.png")
+    let r7:SKTexture = SKTexture(imageNamed: "7.png")
+    let r8:SKTexture = SKTexture(imageNamed: "8.png")
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+//        self.backgroundColor = UIColor.whiteColor()
+        robot = SKSpriteNode(texture: r1)
+        robot.position = CGPoint(x: 100, y: self.frame.size.height/2)
+        robot.size = CGSize(width: robot.size.width*4, height: robot.size.height*4)
+        self.addChild(robot)
         
-        self.addChild(myLabel)
+        let chay:SKAction = SKAction.animateWithTextures([r1,r2,r3,r4,r5,r6], timePerFrame: 0.2)
+        let chathoai:SKAction = SKAction.repeatActionForever(chay)
+        robot.runAction(chathoai)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
         for touch in touches {
-            let location = touch.locationInNode(self)
+//            let location = touch.locationInNode(self)
+            let chay:SKAction = SKAction.animateWithTextures([r7], timePerFrame: 0.2)
+            robot.runAction(chay)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let shoot:SKSpriteNode = SKSpriteNode(texture: r8)
+            shoot.size = CGSize(width: shoot.size.width/4, height: shoot.size.height/4)
+            shoot.position = CGPoint(x: robot.position.x + robot.frame.size.width - 28, y: robot.position.y + 30)
+            self.addChild(shoot)
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+//            let xoay:SKAction = SKAction.rotateByAngle(<#T##radians: CGFloat##CGFloat#>, duration: <#T##NSTimeInterval#>)
+//            shoot.runAction(xoay)
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            let ban:SKAction = SKAction.moveToX(self.frame.size.width + 100, duration: 3)
+            shoot.runAction(ban)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            let sound:SKAction = SKAction.playSoundFileNamed("shoot.mp3", waitForCompletion: false)
+            shoot.runAction(sound)
             
-            self.addChild(sprite)
+            let path = NSBundle.mainBundle().pathForResource("lua", ofType: "sks")
+            let Lua = NSKeyedUnarchiver.unarchiveObjectWithFile(path!) as! SKEmitterNode
+            Lua.position = CGPoint(x: shoot.position.x - 30, y: shoot.position.y)
+            self.addChild(Lua)
+            
         }
     }
    
